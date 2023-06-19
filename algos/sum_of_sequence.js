adaf57389d28ec21c42f30eeacebd4cb648595ac
dcalligy <dcalligy813@gmail.com>
2023-06-18 20:14:42 -0400

    Adding the next problem we plan on solving

---
+++ algos/sum_of_sequence.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+// The sequence is defined by 3 non-negative values:
+// begin, end, step.
+
+// If begin is greater than your end, your function
+// should return 0. If end is not the result of an
+// integer number of steps, then don't add it to the
+// sum. See the 4th example below.
+
+// Examples:
+// 2, 2, 2 --> 2
+// 2, 6, 2 --> 12 (2 + 4 + 6)
+// 1, 5, 2 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1, 5, 3 --> 5 (1 + 4)
+const sequenceSum = (begin, end, step) => {
+  // May the Force be with you
+};
