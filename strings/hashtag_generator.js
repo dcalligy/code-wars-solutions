bfcbf2d025988a9ea25f6b724521625aa0095c34
dcalligy <dcalligy813@gmail.com>
2023-06-21 21:01:31 -0400

    adding another problem

---
+++ strings/hashtag_generator.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+// - It must start with a hashtag (#)
+// - All words must have their first letter capitalized.
+// - If the final result is longer than 140 chars it must return false.
+// - If the input of the final result is an empty string it must return false.
+
+// Examples:
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+function generateHashtag (str) {
+}
