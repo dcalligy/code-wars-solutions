a9267c7f6c0655b7fc6899b27179ebe9d71700b6
dcalligy <dcalligy813@gmail.com>
2023-06-23 22:40:46 -0400

    Potential solution:

--- strings/hashtag_generator.js
+++ strings/hashtag_generator.js
@@ -19,4 +19,18 @@ function generateHashtag (str) {
   // - Capitalize the first letter of each word.  (might need to loop through the array before this step)
   // - Do some join magic to convert our array into a string.
   // - Check the len of the string and return from the function accordingly.
+  if (str.length == 0) {
+    return 0;
+  }
+  const words_arry = str.trim().split(' ');
+  let hash_tag = '#';
+  for (const word of words_arry) {
+    console.log('word[0]: ', word[0]);
+    hash_tag += word[0].toUpperCase() + word.substr(1, (word.length - 1));
+  }
+  console.log('hash_tag: ', hash_tag);
 }
+
+console.log(generateHashtag(' Hello there thanks for trying my Kata'));
+console.log(generateHashtag('    Hello     World   '));
+console.log('');
