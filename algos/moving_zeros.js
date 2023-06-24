35c187cfb27ffdf97ba2435c25a9a3350a906a0e
dcalligy <dcalligy813@gmail.com>
2023-06-24 07:58:18 -0400

    Our next victim

---
+++ algos/moving_zeros.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// Write an algorithm that takes an array
+// and moves all zeros to the end, preserving
+// the order of the other elements.
+
+// Solution:
+// - Loop
+// - pop/push magic?
+function moveZeros(arr) {
+  return
+}
+
+// returns[false,1,1,2,1,3,"a",0,0]
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
