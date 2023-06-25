5a168a43abdf4675283070f48fed177c9a6ac5a3
dcalligy <dcalligy813@gmail.com>
2023-06-24 22:29:59 -0400

    Another victim to my goofy solutions

--- algos/moving_zeros.js
+++ algos/moving_zeros.js
@@ -3,11 +3,17 @@
 // and moves all zeros to the end, preserving
 // the order of the other elements.
 
-// Solution:
-// - Loop
-// - pop/push magic?
 function moveZeros(arr) {
-  return
+  const return_arry = [];
+  const zeros = [];
+  for (const element of arr) {
+    if (element !== 0 || typeof(element) !== 'number') {
+      return_arry.push(element);
+    } else {
+      zeros.push(element);
+    }
+  }
+  return return_arry.concat(zeros);
 }
 
 // returns[false,1,1,2,1,3,"a",0,0]
