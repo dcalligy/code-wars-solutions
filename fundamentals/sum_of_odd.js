7d357955f84d109eeaddb752ea0b7ad840c27e04
dcalligy <dcalligy813@gmail.com>
2023-08-26 00:02:02 -0400

    fixing my triangle

--- fundamentals/sum_of_odd.js
+++ fundamentals/sum_of_odd.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 // Given the triangle of consecutive odd numbers:
-//          1
-//       3     5
-//     7     9    11
+//           1
+//        3     5
+//      7     9    11
 //   13    15    17    19
 // 21    23    25    27    29
 // ...
