fc66296a87577a92a1802d223b0e7654e62d75e0
dcalligy <dcalligy813@gmail.com>
2024-06-22 10:39:19 -0400

    Starting a new problem. Will solve after Luke goes to bed

---
+++ fundamentals/sort_odd.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+// You will be given an array of numbers. You have to stor the odd numbers in ascending order
+// while leaving the even numbers in their original positions.
+// Examples:
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// TODO: Come back with pseudocode after taking care of the baby.
+const sortArray = (array) => {
+  // Return a sorted array.
+  let curr_odd;
+  let prev_odd;
+  for (const ele of array) {
+  }
+}
